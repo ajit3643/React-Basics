af7984a20bc89419ef223200448505afec0ef2a4
Kumar Ajit <ajitinfotech28@gmail.com>
2024-12-06 14:41:05 +0530

    remove vitals

--- Create-React-App/helloworld/src/App.js
+++ Create-React-App/helloworld/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to the React World with - Ajit</h1>
+        <h2>Updated</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
